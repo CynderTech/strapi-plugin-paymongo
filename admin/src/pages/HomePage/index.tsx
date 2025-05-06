f35ba65908f76d3ad31eb01fd29c1ae2b3063f3f
Bea M <64287361+beamariano@users.noreply.github.com>
2025-05-06 15:25:56 +0800

    feat: add paymongo values

--- admin/src/pages/HomePage/index.tsx
+++ admin/src/pages/HomePage/index.tsx
@@ -1,62 +1,127 @@
 import {
 	Box,
 	Button,
+	Card,
 	ContentLayout,
-	GridLayout,
 	HeaderLayout,
 	Main,
+	Stack,
 	TextInput,
+	ToggleInput,
+	Typography,
 } from '@strapi/design-system';
 import { isEmpty } from 'lodash';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const HomePage = () => {
 	const [token, setToken] = useState('');
+
 	return (
 		<Main>
 			<HeaderLayout
 				primaryAction={<Button disabled={isEmpty(token)}>Save</Button>}
 				title="PayMongo Settings"
+				subtitle="Configure PayMongo settings"
 			/>
 			<ContentLayout>
-				<GridLayout>
-					<Box>
-						<TextInput
-							label="Sandbox Public Key"
-							name="sandboxPublicKey"
-							// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-							onChange={(e: any) => setToken(e.target.value)}
-							value={token}
-						/>
-					</Box>
-					<Box>
-						<TextInput
-							label="Sandbox Secret Key"
-							name="sandboxSecretKey"
-							// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-							onChange={(e: any) => setToken(e.target.value)}
-							value={token}
-						/>
-					</Box>
-					<Box>
-						<TextInput
-							label="Live Public Key"
-							name="livePublicKey"
-							// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-							onChange={(e: any) => setToken(e.target.value)}
-							value={token}
-						/>
-					</Box>
-					<Box>
-						<TextInput
-							label="Live Secret Key"
-							name="liveSecretKey"
-							// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-							onChange={(e: any) => setToken(e.target.value)}
-							value={token}
-						/>
-					</Box>
-				</GridLayout>
+				<Card background='white' padding='2rem' hasRadius={true} width='80%' paddingBottom="3rem" marginBottom='3rem'>
+					<Stack size={8}>
+						<Box>
+							<ToggleInput
+								label='Test Mode'
+								onLabel="True"
+								offLabel="False"
+							// checked={checked}
+							// onChange={handleChange}
+							/>
+						</Box>
+						<Box>
+							<Stack size={4}>
+								<Typography variant='beta'>API Keys</Typography>
+								<TextInput
+									label="Company Name"
+									name="companyName"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<TextInput
+									label="Live Public Key"
+									name="livePublicKey"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<TextInput
+									label="Live Secret Key"
+									name="liveSecretKey"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<TextInput
+									label="Test Public Key"
+									name="testPublicKey"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<ToggleInput
+									label='Use redirect for 3DS'
+									onLabel="True"
+									offLabel="False"
+								// checked={checked}
+								// onChange={handleChange}
+								/>
+							</Stack>
+						</Box>
+						<Box>
+							<Stack size={4}>
+								<Typography variant='beta'>Webhook Settings</Typography>
+								<TextInput
+									label="Webhook Signing Secret"
+									name="webhookSigningSecret"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+							</Stack>
+						</Box>
+						<Box>
+							<Stack size={4}>
+								<Typography variant='beta'>Redirect URLs</Typography>
+								<TextInput
+									label="Checkout Success (Web)"
+									name="checkoutSuccessWeb"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<TextInput
+									label="Checkout Failure (Web)"
+									name="checkoutFailureWeb"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<TextInput
+									label="Checkout Success (Mobile)"
+									name="checkoutSuccessMobile"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+								<TextInput
+									label="Checkout Failure (Mobile)"
+									name="checkoutFailureMobile"
+									// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+									onChange={(e: any) => setToken(e.target.value)}
+									value={token}
+								/>
+							</Stack>
+						</Box>
+					</Stack>
+				</Card>
 			</ContentLayout>
 		</Main>
 	);
